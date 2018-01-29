55efbf9c2ddfacac7b495cc012291063205b9323
Raul <tonyrenteria14@gmail.com>
2018-01-29 13:30:31 -0600

    First commit

---
+++ cmake-build-debug/CMakeFiles/Hw1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hw1.dir/sh.c.o"
+  "Hw1.pdb"
+  "Hw1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Hw1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
